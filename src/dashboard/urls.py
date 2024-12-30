91332129b21dd65eccc2851620821e2f7f46d40c
Kostusas <kostasvilkelis@gmail.com>
2024-12-30 04:33:06 +0100

    ensure login

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('', views.dash_page, name='dashboard'),
+    path('', login_required(views.dash_page), name='dashboard'),
 ]
