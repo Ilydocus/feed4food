c8b205ce70e7072d7a9da4168ed209febb57fe6d
Klervie Toczé <klervie.tocze@liu.se>
2025-05-16 14:43:06 +0200

    Implementing sales visualization for the personal dashbaord

--- src/personalDashboard/personalDashboard.py
+++ src/personalDashboard/personalDashboard.py
@@ -1,4 +1,5 @@
 from report.models import ProduceReport, ProduceReportDetails, Item
+from salesReport.models import SalesReport, SalesReportDetails
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
@@ -25,7 +26,7 @@ fig = go.Figure()
 # fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 
-# Fetch data
+# Fetch data - production
 def fetch_user_data(item, user_id):
     user_reports = ProduceReport.objects.filter(user=user_id)
     report_ids = [report.report_id for report in user_reports]
@@ -38,6 +39,35 @@ def fetch_user_data(item, user_id):
         )
         total_quantity.append(sum([x.quantity for x in detailed_reports]))
     return date_produced, total_quantity
+
+# Fetch data - sales
+def fetch_user_data_sales(item, user_id):
+    user_reports = SalesReport.objects.filter(user=user_id)
+    report_ids = [report.report_id for report in user_reports]
+    #timestamps = [report.creation_time for report in user_reports]
+    currency = [report.currency for report in user_reports]
+    #Get the dates of the sales
+    # all_sale_dates = []
+    revenue_per_date = {}
+    for report_id in report_ids:
+        detailed_reports = SalesReportDetails.objects.filter(
+            report_id=report_id, product=item
+        )
+        for x in detailed_reports:
+            revenue_per_date[x.sale_date] = 0.0 #Default revenue
+    #     all_sale_dates.append([x.sale_date for x in detailed_reports])
+    # for date in all_sale_dates:
+    #     revenue_per_date[date] = 0.0 #Default revenue
+    #Get the total revenues per date (and item)
+    #total_revenue_per_date = []
+    for date in list(revenue_per_date.keys()):
+        for report_id in report_ids:
+            detailed_reports = SalesReportDetails.objects.filter(
+                report_id=report_id, product=item, sale_date=date
+            )
+            for x in detailed_reports: #TODO test with two sales of the same product the same day
+                revenue_per_date[date]+= x.quantity*x.price 
+    return list(revenue_per_date.keys()), list(revenue_per_date.values())
 
 
 
@@ -79,7 +109,7 @@ app.layout = dbc.Container(
             style={"margin-bottom": "15px"},
             options=[
                     {'label': 'Production per product', 'value': 'on'},
-                    {'label': 'Sales per product', 'value': 'off'},
+                    {'label': 'Sales per product', 'value': 'on1'},
                     {'label': 'Workforce costs', 'value': 'off2'}
                 ],
         ),
@@ -108,16 +138,26 @@ app.layout = dbc.Container(
 )
 
     # Callbacks
-@app.callback(Output("data-chart", "figure"), [Input("item-selector", "value")])
-def update_graph(value, **kwargs):
-    dates, quantities = fetch_user_data(value, kwargs["user"].id)
-    df = pd.DataFrame({
-    "Day": dates,
-    "Quantity": quantities
-})
-    #fig = go.Figure(data=go.Scatter(x=dates, y=quantities))
-    fig = px.bar(df, x="Day", y="Quantity")
-    #fig.update_layout(yaxis=dict(range=[min(quantities), max(quantities)]))
+@app.callback(Output("data-chart", "figure"), [Input("item-selector", "value"), Input("data-selector", "value")])
+def update_graph(item_value, data_value, **kwargs):
+    if (data_value == 'on'):
+        dates, quantities = fetch_user_data(item_value, kwargs["user"].id)
+        df = pd.DataFrame({
+        "Day": dates,
+        "Quantity": quantities
+        })
+        #fig = go.Figure(data=go.Scatter(x=dates, y=quantities))
+        fig = px.bar(df, x="Day", y="Quantity")
+        #fig.update_layout(yaxis=dict(range=[min(quantities), max(quantities)]))
+    else:
+        if (data_value == 'on1'):
+            dates, sales = fetch_user_data_sales(item_value, kwargs["user"].id)
+            df = pd.DataFrame({
+            "Day": dates,
+            "Sales": sales
+            })
+            fig = px.bar(df, x="Day", y="Sales")
+
     return fig
 
 @app.callback(
@@ -126,7 +166,7 @@ def update_graph(value, **kwargs):
 )
 def chained_callback_product(kpi_selector):
     return (
-        item_options if data-selector == "on" else []
+        item_options if data-selector == "on" or data-selector == "on1" else []
     )
 
     # dff = copy.deepcopy(options)
@@ -144,7 +184,7 @@ def chained_callback_product(kpi_selector):
    Input(component_id='data-selector', component_property='value'))
 
 def show_hide_element(visibility_state):
-    if visibility_state == 'on':
+    if visibility_state == 'on' or visibility_state == 'on1':
         return {"color": "white",'display': 'block'}
     if visibility_state == 'off':
         return {'display': 'none'}
@@ -158,7 +198,7 @@ def show_hide_element(visibility_state):
    [Input(component_id='data-selector', component_property='value')])
 
 def show_hide_element2(visibility_state):
-    if visibility_state == 'on':
+    if visibility_state == 'on' or visibility_state == 'on1':
         return {'display': 'block'}
     if visibility_state == 'off':
         return {'display': 'none'}
@@ -173,7 +213,7 @@ def show_hide_element2(visibility_state):
    Input(component_id='item-selector', component_property='value'))
 
 def show_hide_element3(visibility_state, item_selected):
-    if visibility_state == 'on' and item_selected is not None:
+    if visibility_state == 'on' or visibility_state == 'on1'  and item_selected is not None:
         return {'display': 'block'}
     if visibility_state == 'off':
         return {'display': 'none'}
