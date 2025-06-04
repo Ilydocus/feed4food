53d9b93ec8fdce97a85b7c594ce81d262d4c7b98
Klervie Toczé <klervie.tocze@liu.se>
2025-06-04 17:55:18 +0200

    Fixing AJAX for cultivationReport

--- src/cultivationReport/views.py
+++ src/cultivationReport/views.py
@@ -7,18 +7,6 @@ from django.urls import reverse
 from django.http import JsonResponse
 import json
 
-def get_locations(request):
-    city = request.GET.get('city')
-    locations = LLLocation.objects.filter(living_lab=city).order_by('name')
-    #return JsonResponse(list(locations), safe=False)
-    return render(request, 'hr/locations_dropdown_list_options.html', {'locations': locations})
-
-def get_gardens(request):
-    city = request.GET.get('city')
-    location = request.GET.get('location')
-    gardens = Garden.objects.filter(location_id=location).order_by('name')
-    return render(request, 'hr/gardens_dropdown_list_options.html', {'gardens': gardens})
-
 def get_post_report(request):
     if request.method == "GET":
         report = CultivationReportForm()
