a667547657c5c90277069939f43bd39b8ed00cb8
Klervie Toczé <klervie.tocze@liu.se>
2025-06-10 11:57:11 +0200

    Fixing lower/upper case issue with LLUseReport

--- src/lluseReport/urls.py
+++ src/lluseReport/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("llUseReport", login_required(get_post_report), name="llUseReport"),
+    path("lluseReport", login_required(get_post_report), name="lluseReport"),
     path('get-groups-by-city/', get_groups_by_city, name='get_groups_by_city'),
 ]
