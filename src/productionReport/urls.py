0d932baa48d942256e54d1fc5ea0ace3ef04729e
Klervie Toczé <klervie.tocze@liu.se>
2025-06-10 18:34:27 +0200

    Modifying the GUI of the data portal before the 1.0 release

--- src/productionReport/urls.py
+++ src/productionReport/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("productionReport_form", login_required(get_post_report), name="productionReport_form"),
+    path("productionReport", login_required(get_post_report), name="productionReport"),
     path('ajax/get-locations/', get_locations, name='ajax_get_locations'), 
     path('ajax/get-gardens/', get_gardens, name='ajax_get_gardens'),
     path('get-products-by-city/', get_products_by_city, name='get_products_by_city'), 
