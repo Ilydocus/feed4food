936bae86437fad395c2a8993ab3864101495fc48
Klervie Toczé <klervie.tocze@liu.se>
2025-06-11 18:09:10 +0200

    Adding selected nutrients to the productCategory

--- src/productionReport/models.py
+++ src/productionReport/models.py
@@ -7,7 +7,38 @@ class ProductCategory(models.Model):
         max_length=100, blank=False, null=False, unique=True, primary_key=True
     )
     color = models.CharField(choices=reportUtils.NutrientColors, max_length=100, blank=True)
+    
     energy_kcal = models.IntegerField(default=0)
+    energy_kj = models.IntegerField(default=0)
+
+    vitamin_a = models.FloatField(default=0)
+    vitamin_d = models.FloatField(default=0)
+    vitamin_e = models.FloatField(default=0)
+    vitamin_k = models.FloatField(default=0)
+    vitamin_c = models.FloatField(default=0)
+    vitamin_b1 = models.FloatField(default=0)
+    vitamin_b2 = models.FloatField(default=0)
+    vitamin_b3 = models.FloatField(default=0)
+    vitamin_b6 = models.FloatField(default=0)
+    vitamin_b9 = models.FloatField(default=0)
+    vitamin_b12 = models.FloatField(default=0)
+
+    potassium = models.FloatField(default=0)
+    calcium = models.FloatField(default=0)
+    phosphorus = models.FloatField(default=0)
+    magnesium = models.FloatField(default=0)
+    iron = models.FloatField(default=0)
+    zinc = models.FloatField(default=0)
+    copper = models.FloatField(default=0)
+    selenium = models.FloatField(default=0)
+    iodine = models.FloatField(default=0)
+
+    total_fat = models.FloatField(default=0)
+    saturates = models.FloatField(default=0)
+    carbohydrates = models.FloatField(default=0)
+    sugars = models.FloatField(default=0)
+    protein = models.FloatField(default=0)
+    salt = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
